d64b704c5fa85c5bb8ce739d87efd3b3873d6206
Tirth Patel <tirthshaileshkumar.patel@sjsu.edu>
2021-10-29 04:41:08 +0000

    feat: big data multi-region app codebase

--- 2_UI/src/environments/environment.ts
+++ 2_UI/src/environments/environment.ts
@@ -8,15 +8,15 @@ export const environment = {
 
   // TODO: This id can be retrieved in output section of the cognito ui
   // cloud formation stack.
-  cognitoIdentityPoolId: '',
+  cognitoIdentityPoolId: 'eu-west-1:7db09054-6ff4-4bb7-9946-e1417f1155fd',
 
   // TODO: Facebook app id can be retrieved from the application in your
   // facebook developer account.
-  facebookAppId: '',
+  facebookAppId: '2067849393368589',
 
   // TODO: The API URL is available in the API Gateway console under Stage
   // NOTE: don't forget trailing "/"  For example:
   // https://api.example.com/prod/
-  ticketAPI: ''
+  ticketAPI: 'https://api.geeky-3.me/'
 
 };
